51e646da630b3e804cf282be810b7246e434e95b
Árpád Bűrmen <arpad.buermen@fe.uni-lj.si>
2025-09-16 12:52:39 +0200

    Linking under Windows now works.

--- openvaf/target/src/spec/windows_base.rs
+++ openvaf/target/src/spec/windows_base.rs
@@ -1,9 +1,9 @@
-use crate::spec::{LinkArgs, TargetOptions};
+use crate::spec::{LinkArgs, LinkerFlavor, TargetOptions};
 
 /// Base options for all Windows targets, excluding MSVC-specific arguments.
-pub fn opts_windows_base() -> TargetOptions {
+pub fn opts_windows_base(flavor: LinkerFlavor) -> TargetOptions {
     let pre_link_args = LinkArgs::new();
     let post_link_args = LinkArgs::new();
 
-    TargetOptions { is_like_windows: true, pre_link_args, post_link_args, ..Default::default() }
+    TargetOptions { is_like_windows: true, linker_flavor: flavor, pre_link_args, post_link_args, ..Default::default() }
 }
